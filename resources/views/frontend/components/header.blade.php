ed2f3c39de6cf299da2b5f267ef9a96269f1b5cf
ThisaraAriyawansha <thisara.a2001@gmail.com>
2026-03-10 11:00:33 +0530

    Update header

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -88,7 +88,7 @@
             </nav>
             <div class="menu-end">
                 <div class="bar-menu">
-                  <i class="fa-solid fa-cart-plus"></i>
+                  <i class="fa-solid fa-bars"></i>
                 </div>
                 <a href="javascript:void(0)" id="desktop-menu" class="menu-start">
                   <svg id="ham-menue" viewBox="0 0 100 100"> <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"></path> <path class="line line2" d="M 20,50 H 80"></path> <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"></path> </svg>
@@ -106,18 +106,12 @@
   </div>
     <div class="mobile-nav hmburger-menu" id="mobile-nav" style="display:block;">
         <div class="res-log">
-            <a href="index.html">
+            <a href="{{ route('home') }}" class="logo">
                 <img src="assets/img/logo.png" alt="Responsive Logo" class="white-logo">
             </a>
         </div>
         <ul>
-            <li class="menu-item-has-children"><a href="JavaScript:void(0)">Home</a>
-                <ul class="sub-menu">
-                    <li><a href="index.html">home 1</a></li>
-                    <li><a href="index-2.html">home 2</a></li>
-                    <li><a href="index-3.html">home 3</a></li>
-                </ul>
-            </li>
+            <li><a href="{{ route('home') }}">Home</a></li>
             <li><a href="about.html">about</a></li>
             <li class="menu-item-has-children"><a href="JavaScript:void(0)">pages</a>
                 <ul class="sub-menu">
